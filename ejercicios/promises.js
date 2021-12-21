3d26c5bdc37c573a7d6f7150f31135e10be32f3a
Sebaspatig <sebaspatig@gmail.com>
2021-12-21 11:16:18 -0500

    ejercicio promise feature

--- ejercicios/promises.js
+++ ejercicios/promises.js
@@ -16,7 +16,7 @@ const getPopularMovies = async () => {
 	return data.results;
 };
 
-async function getTopMoviesIds(n = 10) {
+async function getTopMoviesIds(n = 5) {
 	try {
 		const popularMovies = await getPopularMovies();
 		const ids = popularMovies.slice(0, n).map((movie) => movie.id);
@@ -26,18 +26,26 @@ async function getTopMoviesIds(n = 10) {
 	}
 }
 
-// getTopMoviesIds(2).then((ids) => console.log(ids));
+async function getMoviesIds() {
+	try {
+		const popularMovies = await getPopularMovies();
+		const ids = popularMovies.map((movie) => movie.id);
+		return ids;
+	} catch (error) {
+		console.log("tenemos un error", error);
+	}
+}
 
 const renderMovies = (movies) => {
 	const movieList = document.getElementById("movies");
 	movieList.innerHTML = "";
-	console.log(movies)
 	movies.forEach((movie) => {
 		const listElement = document.createElement("li");
+		listElement.setAttribute('class', 'movie__item');
 		listElement.innerHTML = `
 		<img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" />
-		<h5>${movie.title}</h5>
-        <p>Released on <em>${movie.release_date}</em></p>
+		<h5 class="movie__title">${movie.title}</h5>
+        <p class="movie__released">Released on <em>${movie.release_date}</em></p>
 		`;
 
 		movieList.appendChild(listElement);
@@ -45,16 +53,30 @@ const renderMovies = (movies) => {
 };
 
 const getTopMoviesInParallel = async () => {
-
 	const ids = await getTopMoviesIds();
-	const moviePromises = ids.map(id => getMovie(id));
-
+	const moviePromises = ids.map((id) => getMovie(id));
 	const movies = await Promise.all(moviePromises);
 
 	return movies;
 };
 
-document.getElementById('parallel').onclick = async function() {
+const getMovies = async () => {
+	const ids = await getMoviesIds();
+	const moviePromises = ids.map((id) => getMovie(id));
+	const movies = await Promise.all(moviePromises);
+	return movies;
+};
+
+document.getElementById("parallel").onclick = async function () {
 	const movies = await getTopMoviesInParallel();
 	renderMovies(movies);
 };
+document.getElementById("btn_all").onclick = async function () {
+	const movies = await getMovies();
+	renderMovies(movies);
+};
+
+setTimeout(async () => {
+	const movies = await getMovies();
+	renderMovies(movies);
+}, 1000);
