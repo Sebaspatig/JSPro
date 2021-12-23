fc811759ba45f7d224b409bacafafece696e5f3a
Sebaspatig <sebaspatig@gmail.com>
2021-12-23 10:27:01 -0500

    Patron Decorator

---
+++ typescript/ejercicios/ts/PDecorator/index.ts
@@ -0,0 +1,48 @@
+class Field {
+	errors: string[];
+	input: HTMLInputElement;
+
+	constructor(input: HTMLInputElement) {
+		this.input = input;
+		this.errors = [];
+
+		let errorMessage = document.createElement("p");
+		errorMessage.className = "text-danger";
+		this.input.parentNode.insertBefore(errorMessage, this.input.nextSibling);
+
+		this.input.addEventListener("input", () => {
+			this.errors = [];
+			this.validate();
+			errorMessage.innerText = this.errors[0];
+		});
+	}
+
+	validate() {}
+}
+
+function RequireFieldDecorator(field: Field): Field {
+	let validate = field.validate;
+	field.validate = function () {
+		validate();
+		let value = field.input.value;
+		if (!value) {
+			field.errors.push("Campo Requerido");
+		}
+	};
+	return field;
+}
+function EmailFieldDecorator(field: Field): Field {
+	let validate = field.validate;
+	field.validate = function () {
+		validate();
+		let value = field.input.value;
+		if (value.indexOf("@")===-1) {
+			field.errors.push("Debe de ser un email");
+		}
+	};
+	return field;
+}
+
+let field = new Field(document.querySelector("#email"));
+field = RequireFieldDecorator(field);
+field = EmailFieldDecorator(field);
